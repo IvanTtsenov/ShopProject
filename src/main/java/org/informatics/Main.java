742dcf117cc761094b177b836afe9ed792f7cb3e
Ivan <ivan.cenov95@abv.bg>
2026-05-15 17:18:26 +0300

    Second commit test!

--- src/main/java/org/informatics/Main.java
+++ src/main/java/org/informatics/Main.java
@@ -1,8 +1,11 @@
 package org.informatics;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//Alt+F12
+//Git → Commit (Ctrl+K) — вместо git add + git commit
+//Git → Push (Ctrl+Shift+K) — вместо git push
+//Git → Pull — вместо git pull//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Testing commits!");
         }
     }
