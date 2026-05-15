884feb833033d00b5595dc26867f47e1d02540f1
Ivan <ivan.cenov95@abv.bg>
2026-05-15 17:22:17 +0300

    Created data, service and impl packages

--- src/main/java/org/informatics/Main.java
+++ src/main/java/org/informatics/Main.java
@@ -2,8 +2,7 @@ package org.informatics;
 //Alt+F12
 //Git → Commit (Ctrl+K) — вместо git add + git commit
 //Git → Push (Ctrl+Shift+K) — вместо git push
-//Git → Pull — вместо git pull//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Git → Pull — вместо git pull//
 public class Main {
     public static void main(String[] args) {
         System.out.println("Testing commits!");
---
+++ src/main/java/org/informatics/service/impl/Shop.java
@@ -0,0 +1,4 @@
+package org.informatics.service.impl;
+
+public class Shop {
+}
