3fc45500597884c579f37e2556e1c8c7b9792b7e
Ivan <ivan.cenov95@abv.bg>
2026-05-16 20:44:56 +0300

    continuing deserializable logic

--- src/main/java/org/informatics/Main.java
+++ src/main/java/org/informatics/Main.java
@@ -15,16 +15,21 @@ public class Main {
     public static void main(String[] args) {
         Cashier cashier = new Cashier("Ivan",2000);
         Receipt receipt = new Receipt(cashier);
+        Receipt receipt1 = new Receipt(cashier);
+        Receipt receipt2 = new Receipt(cashier);
         String filename = "receipt.ser";
         ReceiptService receiptService = new ReceiptServiceImpl();
         try {
-            receiptService.serializeObj(receipt,filename);
+            //receiptService.serializeObj(receipt,filename);
+           //receiptService.serializeObj(receipt1,filename);
+            receiptService.serializeObj(receipt2,filename);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         try {
-            receiptService.deserializeObj(filename);
+
+            System.out.println( receiptService.deserializeObj(filename));
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (ClassNotFoundException e) {
