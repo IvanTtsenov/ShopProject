30f09bc49f571b04ff41461027739ca193568092
Ivan <ivan.cenov95@abv.bg>
2026-05-18 14:33:38 +0300

    minor changes

--- src/main/java/org/informatics/Main.java
+++ src/main/java/org/informatics/Main.java
@@ -32,29 +32,29 @@ public class Main {
         goodsService.calculateSellingPrice(goods1);
 
         //Cashiers
-        Cashier cashier1 = new Cashier("Ivan",BigDecimal.valueOf(2000));
+        Cashier cashier1 = new Cashier("Ivan", BigDecimal.valueOf(2000));
 
         //CashRegisters
-        CashRegister cashRegister = new CashRegister(cashier1,1);
+        CashRegister cashRegister = new CashRegister(cashier1, 1);
         CashRegisterService cashRegisterService = new CashRegisterServiceImpl();
 
         //Shop
         Shop shop = new Shop();
         ShopService shopService = new ShopServiceImpl(goodsService);
-        shopService.addCashiers(shop,cashier1);
-        shopService.addDeliveredProduct(shop,goods1);
+        shopService.addCashiers(shop, cashier1);
+        shopService.addDeliveredProduct(shop, goods1);
 
         //Receipts
         Receipt receipt1 = new Receipt(cashier1);
         ReceiptService receiptService = new ReceiptServiceImpl(goodsService);
         String filename = "receipt.ser";
-        receiptService.addGoods(receipt1,goods1,BigDecimal.valueOf(5));
+        receiptService.addGoods(receipt1, goods1, BigDecimal.valueOf(5));
         receiptService.calculateTotalPrice(receipt1);
         receiptService.writeFile(receipt1);
         receiptService.readFile(receipt1);
 
         try {
-            receiptService.serializeObj(receipt1,filename);
+            receiptService.serializeObj(receipt1, filename);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -67,49 +67,8 @@ public class Main {
             throw new RuntimeException(e);
         }
 
-
-//        receiptService.calculateTotalPrice(receipt1);
-//        System.out.println("WRITE RECEIPT:");
-//        receiptService.writeFile(receipt1);
-//        receiptService.writeFile(receipt2);
-//        System.out.println("READ RECEIPT:");
-//        receiptService.readFile(receipt1);
-//        System.out.println("!!!!Print receipt1:");
-//        System.out.println(receipt1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-//        Cashier cashier = new Cashier("Ivan",2000);
-//        Receipt receipt = new Receipt(cashier);
-//        Receipt receipt1 = new Receipt(cashier);
-//        Receipt receipt2 = new Receipt(cashier);
-//        String filename = "receipt.ser";
-//        ReceiptService receiptService = new ReceiptServiceImpl();
-//        try {
-//            //receiptService.serializeObj(receipt,filename);
-//           //receiptService.serializeObj(receipt1,filename);
-//            receiptService.serializeObj(receipt2,filename);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        try {
-//
-//            System.out.println( receiptService.deserializeObj(filename));
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        } catch (ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
+        //????????
+        shopService.addReceipt(shop,receipt1);
+        shopService.addDeliveredProduct(shop,goods1);
     }
-    }
+}
